45fa173dc56a3373ba8668588d68ff4e7f089762
Anthony5321 <tyharpo12@gmail.com>
2023-01-21 18:08:50 -0500

    breakable blocks, Code is broken too

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const ball = document.querySelector('.ball')
-const board = document.querySelector('.board')
-
-
+const ball = document.getElementsByClassName('ball')
+const board = document.getElementsByClassName('board')
+const block = document.getElementsByClassName('block')
+const conditions = true;
 
 // function movingBall () {
 //     let leftRight = ()
 
 // }
+
+function breakBlock() {
+        if (!(this.classList.contains("X"))){
+            if (conditions === true) {
+                this.setAttribute("class", "X");
+                winner();
+                conditions = false;
+                return false;
+      
+          }
+            if (conditions === false) {
+                this.setAttribute("class", "");
+                conditions = true;
+          }
+        }
+        
+      }
+
+function winner () {
+
+}
+
+for (math = 0; math < block.length; math += 1){
+    block[math].addEventListener("click", breakBlock)
+}
+
+function playAgain() {
+    bl1.setAttribute("class", "");
+    bl2.setAttribute("class", "");
+    bl3.setAttribute("class", "");
+    bl4.setAttribute("class", "");
+    bl5.setAttribute("class", "");
+    bl6.setAttribute("class", "");
+    bl7.setAttribute("class", "");
+    bl8.setAttribute("class", "");
+    bl9.setAttribute("class", "");
+    bl10.setAttribute("class", "");
+    conditions = true;
+  }
