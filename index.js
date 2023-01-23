97b5421ce6d36325c0ef586eeaec97671e617391
Anthony5321 <tyharpo12@gmail.com>
2023-01-23 15:24:40 -0500

    Ball moved

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const ball = document.getElementsByClassName('ball')
-const board = document.getElementsByClassName('board')
 const block = document.getElementsByClassName('block')
+const paddle = document.getElementsByClassName('paddle')
 const conditions = true;
+let xDirection = -2
+let yDirection = 2
 
-// function movingBall () {
-//     let leftRight = ()
+const ballStart = [305, 40]
+let ballCurrentPosition = ballStart
 
-// }
-
+function userStart () {
+absolute
+  left
+  bottom 
+}
 function breakBlock() {
         if (!(this.classList.contains("x"))){
             if (conditions === true) {
                 this.setAttribute("class", "x");
                 winner();
-                conditions = false;
                 return false;
       
           }
@@ -33,17 +36,48 @@ for (math = 0; math < block.length; math += 1){
     block[math].addEventListener("click", breakBlock)
 }
 
-function playAgain() {
-    bl1.setAttribute("class", "");
-    bl2.setAttribute("class", "");
-    bl3.setAttribute("class", "");
-    bl4.setAttribute("class", "");
-    bl5.setAttribute("class", "");
-    bl6.setAttribute("class", "");
-    bl7.setAttribute("class", "");
-    bl8.setAttribute("class", "");
-    bl9.setAttribute("class", "");
-    bl10.setAttribute("class", "");
-    conditions = true;
-  }
+const board = document.querySelector('.board')
+const ballDiameter = 30
 
+let timerId
+let score = 0
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+board.appendChild(ball)
+
+
+//draw Ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + 'px'
+  ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+
+}
+timerId = setInterval(moveBall, 20)
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2
+    return
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2
+    return
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2
+    return
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2
+    return
+  }
+}
