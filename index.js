e655840dd15be63b0f984dec01e489be1fa8a2d9
Anthony5321 <tyharpo12@gmail.com>
2023-01-25 14:03:35 -0500

    Cleaned it up and Added Color

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const block = document.getElementsByClassName('block')
 const board = document.querySelector('.board')
 const divs = [...document.querySelectorAll('div')]
 const conditions = true;
-let rightPressed = false;
-let leftPressed = false;
-let x = 2
-let y = -2
 const paddleWidth = 75
 const paddleHeight = 15
 const boundsHeight = 640
@@ -13,54 +9,40 @@ const boundsWidth = 540
 const ballDiameter = 15
 const blockHeight = 15
 const blockWidth = 50
-let timerId
-timerId = setInterval(moveBall, 100000)
 const paddleStart = [235, 10]
-let currentPosition = paddleStart
 const ballStart = [265, 40]
-let ballPosition = ballStart
 const rowOne = document.querySelector('.rowOne')
-
+let rightPressed = false;
+let leftPressed = false;
+let x = 2
+let y = -2
+let ballPosition = ballStart
+let currentPosition = paddleStart
+let timerId
+timerId = setInterval(moveBall, 100)
 
 // Key input
 document.onkeydown = (e) => {
   e = e || window.event;
-  if (e.key === 'ArrowLeft') {
-    currentPosition[0] -= 10
-    console.log(currentPosition[0])
-   if (currentPosition[0] <= 0) {
-    currentPosition[0] += 10
-   }
-    movePaddle()
-    console.log(currentPosition[0] > 0)
-    console.log('left arrow pressed')
-  } else if (e.key === 'ArrowRight') {
-    currentPosition[0] += 10
-    console.log(currentPosition[0])
-    if (currentPosition[0] >= 475) {
+  if  (e.key === 'ArrowLeft') {
       currentPosition[0] -= 10
+    if  (currentPosition[0] <= 0) {
+        currentPosition[0] += 10
     }
     movePaddle()
-    console.log('right arrow pressed')
+  } else if (e.key === 'ArrowRight') {
+    currentPosition[0] += 10
+    if  (currentPosition[0] >= 475) {
+        currentPosition[0] -= 10
+    }
+    movePaddle()
   }
-}
-
-
-// For tests
-function breakBlock() {
-        if (!(this.classList.contains(""))){
-                this.remove("class");
-                winner();
-            }
-          }
-for (math = 0; math < block.length; math += 1){
-    block[math].addEventListener("click", breakBlock)
 }
 
 // Add paddle
 const paddle = document.createElement('div')
-paddle.classList.add('paddle')
-board.appendChild(paddle)
+  paddle.classList.add('paddle')
+  board.appendChild(paddle)
 
 // Player moving
 function movePaddle() {
@@ -70,28 +52,23 @@ function movePaddle() {
 
 //add ball
 const ball = document.createElement('div')
-ball.classList.add('ball')
-board.appendChild(ball)
-
+  ball.classList.add('ball')
+  board.appendChild(ball)
 
 //draw Ball
 function drawBall() {
   ball.style.left = ballPosition[0] + 'px'
   ball.style.bottom = ballPosition[1] + 'px'
-
-  console.log(ball.getBoundingClientRect())
 }
 
 //move ball
 function moveBall() {
     ballPosition[0] += x
     ballPosition[1] += y
-ball.getBoundingClientRect();
+    ball.getBoundingClientRect();
     drawBall()
     collision ()
     lose()
-    // collisionBlock()
-
 }
 
 // Reverse Direction
@@ -114,8 +91,7 @@ function change() {
   }
 }
 
-// for testing the blocks
-// const block = document.getElementsByid('bl4')
+//block Detection
 let bl11 = document.getElementById('bl1-1')
 bl11.getBoundingClientRect();
 let bl12 = document.getElementById('bl1-2')
@@ -207,457 +183,180 @@ bl58.getBoundingClientRect();
 let bl59 = document.getElementById('bl5-9')
 bl59.getBoundingClientRect();
 
-
+// collision aware
 function checkCollision(ball, block) {
-  var ballRect = ball.getBoundingClientRect();
-  var blockRect = block.getBoundingClientRect();
-
+  let ballRect = ball.getBoundingClientRect();
+  let blockRect = block.getBoundingClientRect();
   return (ballRect.right >= blockRect.left &&
-      ballRect.left <= blockRect.right) &&
+    ballRect.left <= blockRect.right) &&
     (ballRect.bottom >= ballRect.top &&
-      ballRect.top <= blockRect.bottom);
+    ballRect.top <= blockRect.bottom);
 }
 
 // wall, block and paddle interactions
 function collision () {
-    if(ballPosition[0] >= (boundsWidth - ballDiameter) || ballPosition[0] <= 0 || ballPosition[1] >= (boundsHeight - ballDiameter))
-    {
-      change()
-    }
-    if
-    (
-      (ballPosition[0] > currentPosition[0] && ballPosition[0] < currentPosition[0] + paddleWidth) &&
-      (ballPosition[1] > currentPosition[1] && ballPosition[1] < currentPosition[1] + paddleHeight ) 
-    )
-    {
-      change()
-    }
-    if
-    (
-      checkCollision(ball, bl11)
-    )
-    {
-      bl11.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl12)
-    )
-    {
-      bl12.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl13)
-    )
-    {
-      bl13.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl14)
-    )
-    {
-      bl14.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl15)
-    )
-    {
-      bl15.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl16)
-    )
-    {
-      bl16.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl17)
-    )
-    {
-      bl17.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl18)
-    )
-    {
-      bl18.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl19)
-    )
-    {
-      bl19.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl21)
-    )
-    {
-      bl21.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl22)
-    )
-    {
-      bl22.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl23)
-    )
-    {
-      bl23.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl24)
-    )
-    {
-      bl24.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl25)
-    )
-    {
-      bl25.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl26)
-    )
-    {
-      bl26.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl27)
-    )
-    {
-      bl27.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl28)
-    )
-    {
-      bl28.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl29)
-    )
-    {
-      bl29.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl31)
-    )
-    {
-      bl31.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl32)
-    )
-    {
-      bl32.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl33)
-    )
-    {
-      bl33.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl34)
-    )
-    {
-      bl34.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl35)
-    )
-    {
-      bl35.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl36)
-    )
-    {
-      bl36.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl37)
-    )
-    {
-      bl37.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl38)
-    )
-    {
-      bl38.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl39)
-    )
-    {
-      bl39.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl41)
-    )
-    {
-      bl41.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl42)
-    )
-    {
-      bl42.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl43)
-    )
-    {
-      bl43.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl44)
-    )
-    {
-      bl44.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl45)
-    )
-    {
-      bl45.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl46)
-    )
-    {
-      bl46.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl47)
-    )
-    {
-      bl47.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl48)
-    )
-    {
-      bl48.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl49)
-    )
-    {
-      bl49.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl51)
-    )
-    {
-      bl51.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl52)
-    )
-    {
-      bl52.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl53)
-    )
-    {
-      bl53.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl54)
-    )
-    {
-      bl54.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl55)
-    )
-    {
-      bl55.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl56)
-    )
-    {
-      bl56.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl57)
-    )
-    {
-      bl57.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl58)
-    )
-    {
-      bl58.remove()
-      winner()
-    change()
-    }
-    if
-    (
-      checkCollision(ball, bl59)
-    )
-    {
-      bl59.remove()
-      winner()
-    change()
-    }
+  if(ballPosition[0] >= (boundsWidth - ballDiameter) || ballPosition[0] <= 0 || ballPosition[1] >= (boundsHeight - ballDiameter))
+    {change()}
+  if
+    ((ballPosition[0] > currentPosition[0] && ballPosition[0] < currentPosition[0] + paddleWidth) &&
+    (ballPosition[1] > currentPosition[1] && ballPosition[1] < currentPosition[1] + paddleHeight ))
+    {change()}
+  if
+    (checkCollision(ball, bl11))
+    {bl11.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl12))
+    {bl12.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl13))
+    {bl13.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl14))
+    {bl14.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl15))
+    {bl15.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl16))
+    {bl16.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl17))
+    {bl17.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl18))
+    {bl18.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl19))
+    {bl19.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl21))
+    {bl21.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl22))
+    {bl22.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl23))
+    {bl23.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl24))
+    {bl24.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl25))
+    {bl25.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl26))
+    {bl26.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl27))
+    {bl27.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl28))
+    {bl28.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl29))
+    {bl29.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl31))
+    {bl31.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl32))
+    {bl32.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl33))
+    {bl33.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl34))
+    {bl34.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl35))
+    {bl35.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl36))
+    {bl36.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl37))
+    {bl37.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl38))
+    {bl38.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl39))
+    {bl39.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl41))
+    {bl41.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl42))
+    {bl42.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl43))
+    {bl43.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl44))
+    {bl44.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl45))
+    {bl45.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl46))
+    {bl46.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl47))
+    {bl47.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl48))
+    {bl48.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl49))
+    {bl49.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl51))
+    {bl51.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl52))
+    {bl52.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl53))
+    {bl53.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl54))
+    {bl54.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl55))
+    {bl55.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl56))
+    {bl56.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl57))
+    {bl57.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl58))
+    {bl58.remove(), winner(), change()}
+  if
+    (checkCollision(ball, bl59))
+    {bl59.remove(), winner(), change()}
 }
 
 // Win conditions
 function winner() {
   if (
     (document.querySelector('.rowOne').children.length === 0) && 
-  (document.querySelector('.rowTwo').children.length === 0) &&
-  (document.querySelector('.rowThree').children.length === 0) &&
-  (document.querySelector('.rowFour').children.length === 0) &&
-  (document.querySelector('.rowFive').children.length === 0)
-  )
-             {
-      alert("You win, YAY");
-      location.reload()
-              }
+    (document.querySelector('.rowTwo').children.length === 0) &&
+    (document.querySelector('.rowThree').children.length === 0) &&
+    (document.querySelector('.rowFour').children.length === 0) &&
+    (document.querySelector('.rowFive').children.length === 0)
+    )
+    {
+    alert("You win, YAY");
+    location.reload()
+    }
 }
 
 // Loss conditions
 function lose() {
-  if  (ballPosition[1] < -2) {
-alert('You lost, try again?')
-location.reload()
+  if  (ballPosition[1] === -2) {
+  alert('You lost, try again?')
+  location.reload()
   }
 }
